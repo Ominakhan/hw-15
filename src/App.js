d15635925f7caa44177d6448061d682ad9c9d0f5
Omina <omina.mamatalieva.ch@gmail.com>
2022-10-12 00:51:24 +0600

    added user

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <div>Hello!</div>
+    <span>Omina Mamatalieva</span>
+    </div>
+  );
+}
+
+export default App;
